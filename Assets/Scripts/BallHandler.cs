ba41e90eefbf1dcf7797c96d36086a6aacf72c9b
jaimetjr <jaime.tasca.jr@gmail.com>
2021-06-28 01:05:35 -0300

    add the rest of the game and the cinemachine

--- Assets/Scripts/BallHandler.cs
+++ Assets/Scripts/BallHandler.cs
@@ -5,29 +5,70 @@ using UnityEngine.InputSystem;
 
 public class BallHandler : MonoBehaviour
 {
-    [SerializeField] private Rigidbody2D _currentBallRigidbody;
+    [SerializeField] private GameObject _ballPrefab;
+    [SerializeField] private Rigidbody2D _pivot;
+    [SerializeField] private float _delayDuration = 0.15f;
+    [SerializeField] private float _respawnDelay = 1f;
+    
+    private Rigidbody2D _currentBallRigidbody;
+    private SpringJoint2D _currentBallSpringJoint;
     private Camera _mainCamera;
+    private bool isDragging;
     // Start is called before the first frame update
     void Start()
     {
         _mainCamera = Camera.main;
+        SpawnBall();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (_currentBallRigidbody == null)
+            return;
+
         if (!Touchscreen.current.primaryTouch.press.isPressed)
         {
-            _currentBallRigidbody.isKinematic = false;
+            if (isDragging)
+            {
+                LaunchBall();
+            }
+            isDragging = false;
             return;
         }
 
+        isDragging = true;
         _currentBallRigidbody.isKinematic = true;
         Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
 
         Vector3 worldPosition = _mainCamera.ScreenToWorldPoint(touchPosition);
 
         _currentBallRigidbody.position = worldPosition;
+    }
+
+    private void LaunchBall()
+    {
+        _currentBallRigidbody.isKinematic = false;
+        _currentBallRigidbody = null;
+
+        Invoke(nameof(DetachBall), _delayDuration);
+    }
+
+    private void DetachBall()
+    {
+        _currentBallSpringJoint.enabled = false;
+        _currentBallSpringJoint = null;
+
+        Invoke(nameof(SpawnBall), _respawnDelay);
+    }
+
+    private void SpawnBall()
+    {
+        GameObject ballInstance = Instantiate(_ballPrefab, _pivot.position, Quaternion.identity);
+
+        _currentBallRigidbody = ballInstance.GetComponent<Rigidbody2D>();
+        _currentBallSpringJoint = ballInstance.GetComponent<SpringJoint2D>();
+        _currentBallSpringJoint.connectedBody = _pivot;
 
     }
 }
